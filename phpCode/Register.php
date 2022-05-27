019715e13d46a08d80549e2f0f1168fc44ba47cb
judy <judy8889107@gmail.com>
2022-05-27 21:12:22 +0800

    123

--- phpCode/Register.php
+++ phpCode/Register.php
@@ -1,31 +1,63 @@
-<!-- 用戶註冊 -->
+
 <?php
 
 //建立連結
-require_once("./inc/LoginDB.inc");
+// $link = require_once("./inc/LoginDB.inc");
+
 
 //取得表單資料
-$nickName = $_POST["nickName"];
-$userName = $_POST["userName"];
-$password = $_POST["password"];
-$gender = $_POST["gender"];
-$phoneNumber = $_POST["phoneNumber"];
-$email = $_POST["email"];
+$nickName = isset($_POST["nickName"])? $_POST["nickName"]: "";
+$userName = isset($_POST["userName"])? $_POST["userName"]: "";
+$password = isset($_POST["password"])? $_POST["password"]: "";
+$gender = isset($_POST["gender"])? $_POST["gender"]: "";
+$birthday = isset($_POST["birthday"])? $_POST["birthday"]: "";
+$phoneNumber = isset($_POST["phoneNumber"])? $_POST["phoneNumber"]: "";
+$email = isset($_POST["email"])? $_POST["email"]: "";
+
+// if($link) print "ok";
+
+// //insert前確認資料有無重複
+// $Rep = array("帳號"=>false, "手機號碼"=>false, "電子郵件"=>false);
+// // $userNameRep = false;
+// // $phoneNumberRep = false;
+// // $emailRep = false;
+// $isRep = false;
+// $sql = "SELECT userName, phoneNumber, email FROM userdb";
+// $result = mysqli_query($link, $sql);
+// while($row = mysqli_fetch_row($result)){
+//     if($row[0] == $userName) $Rep['帳號'] = true;
+//     if($row[1] == $phoneNumber) $Rep['手機號碼'] = true;
+//     if($row[2] == $email) $Rep['電子郵件'] = true;
+//     if($Rep['帳號'] || $Rep['手機號碼'] || $Rep['電子郵件']){
+//         $isRep = true;
+//         // 有可能帳號一樣，手機號碼也跟別人相同，但是不同列，所以不能break
+//     }
+// }
 
 
+// //插入資料
+// if(!$isRep){
+//     $sql = "INSERT INTO userdb (nickName, userName, password, gender, birthday, phoneNumber, email) 
+//     VALUES ('$nickName', '$userName', '$password', '$gender', '$birthday', '$phoneNumber', '$email')";
+//     mysqli_query($link,$sql);
+// }
 
-//測試
-print "姓名: ".$nickName."<br/>";
-print "帳號: ".$userName."<br/>";
-print "密碼: ".$password."<br/>";
-print "性別: ".$gender."<br/>";
-print "聯絡號碼: ".$phoneNumber."<br/>";
-print "Email: ".$email."<br/>";
 
+// print 3;
+
+// 測試用輸出
+// echo $nickName."<br>";
+// echo $userName."<br>";
+// echo $password."<br>";
+// echo $gender."<br>";
+// echo $birthday."<br>";
+// echo $phoneNumber."<br>";
+// echo $email."<br>";
 
 // 關閉資料庫連結
-require_once("./inc/CloseDB.inc");
-
-
+// mysqli_close($link);
 
 ?>
+
+
+ 
