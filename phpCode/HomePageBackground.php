031b439322397e0eeb5402300b022753b400fb4d
judy <judy8889107@gmail.com>
2022-05-28 21:50:53 +0800

    session完成

--- phpCode/HomePageBackground.php
+++ phpCode/HomePageBackground.php
@@ -4,7 +4,8 @@
     if (isset($_SESSION['islogin'])) {
     // 若已經登入
     echo "你好! ".$_SESSION['userName'].' ,歡迎來到個人中心!<br>';
-    echo "登出";
+    echo "F5重新整理即可登出";
+    session_destroy(); /* 清除session */
     } else {
     // 若沒有登入
     echo "您還沒有登入,請<a href='./SubPage/Login.html'>登入</a>";
