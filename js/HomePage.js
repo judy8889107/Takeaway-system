b1d59c511f4c8f51fd41df6ac88f3f13ef609ca9
judy <judy8889107@gmail.com>
2022-05-30 17:58:44 +0800

    加入機器人元件 (chatFrame, BotmanServer, homepage要放在同一個路徑下)

--- js/HomePage.js
+++ js/HomePage.js
@@ -1,11 +1,12 @@
 var botmanWidget = {
     frameEndpoint: 'chatFrame.html', // 機器人聊天室樣式html
     chatServer: 'BotmanServer.php',
-    title: '你的愛供偎小助理',
-    introMessage: '你好，今天有什麼可以幫你的嗎&#129299;？',
+    title: '你的點餐小助理🍽️',
+    introMessage: '你好，今天有什麼可以幫你的嗎😄？',
     placeholderText: '輸入內容',
-    mainColor: '#F28240',
-    bubbleBackground: '#F28240',
+    bubbleAvatarUrl:'https://i.pinimg.com/originals/47/42/d6/4742d635ccbae64af1f9e67890df8d4f.png', /* Bubble圖片 */
+    mainColor: '#C7C49F',
+    bubbleBackground: '#324238',
     desktopHeight: 800, //調整開啟高度
     aboutLink: 'https://botman.io',
     aboutText: 'BotMan WebSite',
