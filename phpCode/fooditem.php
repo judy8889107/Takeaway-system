ea9f1f33c53535dfd170b2ffb8bce7fce97c191c
judy <judy8889107@gmail.com>
2022-06-09 21:22:46 +0800

    改小細節

--- phpCode/fooditem.php
+++ phpCode/fooditem.php
@@ -4,20 +4,25 @@
 $link = require_once("../phpCode/inc/LoginDB.inc");
 // if($link) echo '<script type="text/javascript">alert("連接資料庫成功")</script>';
 
-$sql = "SELECT id, name, category, rating, price, quantity, img, cal FROM fooditem";
+$sql = "SELECT * FROM fooditem";
 $result = mysqli_query($link, $sql);
-$label = array("id", "name", "category", "rating", "price", "quantity", "img", "cal");
+$label = array();
 $arr = array();
 
+//取得attribute
+$fieldInfo = mysqli_fetch_fields($result);
+foreach($fieldInfo as $val){
+    array_push($label,$val->name);
+}
 
 while ($row = mysqli_fetch_row($result)) {
     $tmp = array();
-   for($i=0;$i<8;$i++){
+   for($i=0;$i<count($label);$i++){
        $tmp[$label[$i]] = $row[$i];
    }
    array_push($arr,$tmp);
 }
-
+//回傳json檔
 $json_arr = json_encode($arr);
 print $json_arr;
 
