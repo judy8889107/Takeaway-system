f81a30e6f3b849f6af51db75461b5b998295d61b
judy <judy8889107@gmail.com>
2022-06-22 19:49:00 +0800

    修復講笑話&&迷因

--- phpCode/test.php
+++ phpCode/test.php
@@ -1,19 +1,22 @@
 <?php
 //測試抓取遠端IP
-if(!empty($_SERVER["HTTP_CLIENT_IP"]))
-    	{
-    		$cip = $_SERVER["HTTP_CLIENT_IP"];
-    	}
-    	else if(!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
-    	{
-    		$cip = $_SERVER["HTTP_X_FORWARDED_FOR"];
-    	}
-    	else if(!empty($_SERVER["REMOTE_ADDR"]))
-    	{
-    		$cip = $_SERVER["REMOTE_ADDR"];
-    	}
-    	else
-    	{
-    		$cip = '';
-    	}
-echo $cip;
+if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
+    $cip = $_SERVER["HTTP_CLIENT_IP"];
+} else if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+    $cip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+} else if (!empty($_SERVER["REMOTE_ADDR"])) {
+    $cip = $_SERVER["REMOTE_ADDR"];
+} else {
+    $cip = '';
+}
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    $url = "https://";
+else
+    $url = "http://";
+// Append the host(domain name, ip) to the URL.   
+$url .= $_SERVER['HTTP_HOST'];
+
+// Append the requested resource location to the URL   
+// $url .= $_SERVER['REQUEST_URI'];
+
+echo $url;
